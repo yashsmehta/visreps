fa5c233082ccaad2b9cb12dca42583c7d27a8bf9
Yash Mehta <yashsmehta95@gmail.com>
2024-04-08 14:06:53 -0400

    Update config dict params

--- archvision/runner.py
+++ archvision/runner.py
@@ -10,13 +10,20 @@ import archvision.evals as evals
 if __name__ == "__main__":
 
     cfg_dict = {
-        "voxel_set": "OTC",  # region of interest in the data
-        "eval_metric": "ersa",
-        "benchmark_data_type": "fMRI",
-        "model_uid": "torchvision_alexnet_imagenet1k_v1",
-        "log_expdata": True,  # flag to save the training data
-        "log_dir": "logs/",  # directory to save experimental data
-        "exp_name": "test",  # logs are stored under a directory created under this name
+    "wavelet": {
+        "layers": 1,
+        "type": "gabor", # haar, db2, morlet, gaussian, sobel
+    },
+    "conv": {
+        "layers": 4,
+        "console": True,
+        },
+    "voxel_set": "OTC",
+    "eval_metric": "srpr",
+    "benchmark_data_type": "fMRI",
+    "log_expdata": True,  # flag to save the training data
+    "log_dir": "logs/",  # directory to save experimental data
+    "exp_name": "test",  # logs are stored under a directory created under this name
     }
 
     cfg = OmegaConf.create(cfg_dict)
