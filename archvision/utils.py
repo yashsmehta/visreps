9d1b9dd7bf4a8dff95980f3e89ae84e5b80f5621
Yash Mehta <yashsmehta95@gmail.com>
2024-04-10 01:04:37 -0400

    Add utils for checking + merge config

--- archvision/utils.py
+++ archvision/utils.py
@@ -1,3 +1,33 @@
-"""
-this file contains all the utils functions
-"""
+
+def validate_and_update_config(cfg):
+    """
+    asserts that the configuration is valid, and adds the default layer values.
+    """
+    assert cfg.nonlin in [
+        "linear",
+        "relu",
+        "tanh",
+        "sigmoid",
+    ], "only linear, relu, tanh, sigmoid supported!"
+    assert cfg.init in [
+        "kaiming",
+        "xavier",
+        "gaussian",
+        "uniform",
+    ], "only kaiming, xavier, gaussian, uniform supported!"
+    assert cfg.norm in [
+        "batch",
+        "instance",
+        "channel",
+        "none",
+    ], "only batch, instance, channel, none supported!"
+    default_layer = {
+        "kernel_size": 3,
+        "channels": 64,
+        "pooling": "none",
+        "pool_kernel_size": 2,
+    }
+    for i, layer in enumerate(cfg.model.layers):
+        cfg.model.layers[i] = {**default_layer, **layer}
+
+    return cfg
