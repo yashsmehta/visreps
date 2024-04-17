36a75b183fd14a41cdfcc86a14bdbea46e5955a2
Yash Mehta <yashsmehta95@gmail.com>
2024-04-17 00:13:25 -0400

    Add naming to last layer of network

--- archvision/models/conv_layers.py
+++ archvision/models/conv_layers.py
@@ -7,20 +7,29 @@ class ConvolutionLayers(nn.Module):
         super(ConvolutionLayers, self).__init__()
 
         self.conv_layers = nn.ModuleList()
-        self.n_channels = in_channels if cfg.model.conv_in_channels == "none" else cfg.model.conv_in_channels
+        self.n_channels = (
+            in_channels
+            if cfg.model.conv_in_channels == "none"
+            else cfg.model.conv_in_channels
+        )
         self.device = device
 
-        for layer in cfg.model.layers:
-            conv_layer = nn.Conv2d(in_channels, layer.channels, kernel_size=layer.kernel_size, padding=1).to(self.device)
+        for i, layer in enumerate(cfg.model.layers):
+            conv_layer = nn.Conv2d(
+                in_channels, layer.channels, kernel_size=layer.kernel_size, padding=1
+            ).to(self.device)
             self.initialize_weights(conv_layer, cfg.model.weights_init)
 
             nonlinearity = self.get_nonlinearity(cfg.model.nonlin)
             normalization = self.get_normalization(layer.channels, cfg.model.norm)
             pooling = self.get_pooling(layer.pooling, layer.pool_kernel_size)
-            
-            self.conv_layers.append(nn.Sequential(conv_layer, nonlinearity, normalization, pooling))
-            in_channels = layer.channels
+            if i == len(cfg.model.layers) - 1:
+                conv_layer.__class__.__name__ = "last_layer"
 
+            self.conv_layers.append(
+                nn.Sequential(conv_layer, nonlinearity, normalization, pooling)
+            )
+            in_channels = layer.channels
 
     def forward(self, x):
         x = x.to(self.device)
@@ -30,39 +39,39 @@ class ConvolutionLayers(nn.Module):
 
     def get_normalization(self, out_channels, norm_type):
         match norm_type:
-            case 'batch':
+            case "batch":
                 return nn.BatchNorm2d(out_channels)
-            case 'channel':
+            case "channel":
                 return DivNorm()
-            case 'instance':
+            case "instance":
                 return nn.InstanceNorm2d(out_channels)
-            case 'none':
+            case "none":
                 return nn.Identity()
             case _:
                 raise ValueError(f"Unsupported normalization method: {norm_type}")
 
     def get_nonlinearity(self, nonlin_type):
         match nonlin_type:
-            case 'relu':
+            case "relu":
                 return nn.ReLU(inplace=True)
-            case 'tanh':
+            case "tanh":
                 return nn.Tanh()
-            case 'sigmoid':
+            case "sigmoid":
                 return nn.Sigmoid()
-            case 'elu':
+            case "elu":
                 return nn.ELU(inplace=True)
-            case 'none':
+            case "none":
                 return nn.Identity()
             case _:
                 raise ValueError(f"Unsupported non-linearity: {nonlin_type}")
 
     def get_pooling(self, pool_type, pool_kernel_size):
         match pool_type:
-            case 'max':
+            case "max":
                 return nn.MaxPool2d(kernel_size=pool_kernel_size)
-            case 'avg':
+            case "avg":
                 return nn.AvgPool2d(kernel_size=pool_kernel_size)
-            case 'none':
+            case "none":
                 return nn.Identity()
             case _:
                 raise ValueError(f"Unsupported pool type: {pool_type}")
@@ -79,7 +88,3 @@ class ConvolutionLayers(nn.Module):
                 nn.init.uniform_(conv_layer.weight, a=-0.02, b=0.02)
             case _:
                 raise ValueError(f"Unsupported initialization method: {initialization}")
-
-
-
-    
