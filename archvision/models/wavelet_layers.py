68b64a9bf306c9f3f9c9831cf33ec4f8259b3659
Yash Mehta <yashsmehta95@gmail.com>
2024-04-17 00:13:50 -0400

    Simply wavelet_layer to match usage in ExpansionNet

--- archvision/models/wavelet_layers.py
+++ archvision/models/wavelet_layers.py
@@ -3,29 +3,27 @@ import os
 from models.custom_operations.convolution import WaveletConvolution
 from models.custom_operations.norm import DivNorm
 
-        
-        
+
 class WaveletLayers(nn.Module):
     def __init__(self, cfg, device):
         super(WaveletLayers, self).__init__()
 
-        
         self.wavelet_layers = nn.ModuleList()
         self.device = device
-        self.pooling = 'avg'
-        self.pool_kernel_size = 3
-        
 
-        wavelet_layer = WaveletConvolution(filter_type=cfg.model.wavelet_type,
-                                           device=self.device).to(self.device)
+        wavelet_layer = WaveletConvolution(
+            filter_size=15,
+            filter_type="curvature",
+            filter_params=cfg.model.wavelet,
+            device=self.device,
+        ).to(self.device)
         self.out_channels = wavelet_layer.layer_size
+        print("wavelet layer size: ", self.out_channels)
 
-        
         nonlinearity = self.get_nonlinearity(cfg.model.nonlin)
-        normalization = self.get_normalization(self.out_channels, cfg.model.norm)
-        pooling = self.get_pooling(self.pooling, self.pool_kernel_size)
-            
-        self.wavelet_layers.append(nn.Sequential(wavelet_layer, nonlinearity, normalization, pooling))
+        pooling = self.get_pooling("avg", 2)
+
+        self.wavelet_layers.append(nn.Sequential(wavelet_layer, nonlinearity, pooling))
 
     def forward(self, x):
         x = x.to(self.device)
@@ -35,41 +33,41 @@ class WaveletLayers(nn.Module):
 
     def get_normalization(self, out_channels, norm_type):
         match norm_type:
-            case 'batch':
+            case "batch":
                 return nn.BatchNorm2d(out_channels)
-            case 'channel':
+            case "channel":
                 return DivNorm()
-            case 'instance':
+            case "instance":
                 return nn.InstanceNorm2d(out_channels)
-            case 'layer':
+            case "layer":
                 return nn.LayerNorm(out_channels)
-            case 'none':
+            case "none":
                 return nn.Identity()
             case _:
                 raise ValueError(f"Unsupported normalization method: {norm_type}")
 
     def get_nonlinearity(self, nonlin_type):
         match nonlin_type:
-            case 'relu':
+            case "relu":
                 return nn.ReLU(inplace=True)
-            case 'tanh':
+            case "tanh":
                 return nn.Tanh()
-            case 'sigmoid':
+            case "sigmoid":
                 return nn.Sigmoid()
-            case 'elu':
+            case "elu":
                 return nn.ELU(inplace=True)
-            case 'none':
+            case "none":
                 return nn.Identity()
             case _:
                 raise ValueError(f"Unsupported non-linearity: {nonlin_type}")
 
     def get_pooling(self, pool_type, pool_kernel_size):
         match pool_type:
-            case 'max':
+            case "max":
                 return nn.MaxPool2d(kernel_size=pool_kernel_size)
-            case 'avg':
+            case "avg":
                 return nn.AvgPool2d(kernel_size=pool_kernel_size)
-            case 'none':
+            case "none":
                 return nn.Identity()
             case _:
                 raise ValueError(f"Unsupported pool type: {pool_type}")
@@ -86,13 +84,3 @@ class WaveletLayers(nn.Module):
                 nn.init.uniform_(conv_layer.weight, a=-0.02, b=0.02)
             case _:
                 raise ValueError(f"Unsupported initialization method: {initialization}")
-
-
-
-
-
-
-
-
-
-    
