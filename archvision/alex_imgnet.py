7d290ace425aa38d6478cab596561f2c8a5ea3b9
Yash Mehta <yashsmehta95@gmail.com>
2024-04-30 18:57:14 -0400

    updated alex_imgnet trainer

--- archvision/alex_imgnet.py
+++ archvision/alex_imgnet.py
@@ -4,17 +4,16 @@ import torch.optim as optim
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 
-# Parameters
-num_classes = 200  # Tiny ImageNet has 200 classes
-batch_size = 64
-num_epochs = 10
+
+num_classes = 200
+batch_size = 128
+num_epochs = 20
 learning_rate = 0.001
 
-# Data transformations
 transform = transforms.Compose([
-    transforms.Resize((64, 64)),  # Resize images to 64x64
+    transforms.Resize((64, 64)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet stats
+    transforms.Normalize(mean=[0.480, 0.448, 0.398], std=[0.272, 0.265, 0.274]) # these are the mean and std of the tiny-ImageNet dataset
 ])
 
 train_dataset = datasets.ImageFolder(root='../data/tiny-imagenet-200/train', transform=transform)
@@ -24,43 +23,10 @@ train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 
-class AlexNet(nn.Module):
-    def __init__(self, num_classes=200):
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
+model = models.alexnet()
+model.classifier[-1] = nn.Linear(model.classifier[-1].in_features, num_classes)
+print(model)
 
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(256 * 1 * 1, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
-
-
-model = AlexNet(num_classes=num_classes)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = model.to(device)
 
@@ -75,7 +41,7 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 for epoch in range(num_epochs):
     model.train()
     for i, (images, labels) in enumerate(train_loader):
-        images, labels = images.to('cuda'), labels.to('cuda')
+        images, labels = images.to(device), labels.to(device)
         outputs = model(images)
         loss = criterion(outputs, labels)
 
@@ -84,16 +50,19 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         if (i+1) % 100 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.6f}')
 
     model.eval()
     with torch.no_grad():
         correct = 0
         total = 0
         for images, labels in test_loader:
-            images, labels = images.to('cuda'), labels.to('cuda')
+            images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
         print(f'Accuracy of the model on the test images: {100 * correct / total:.2f}%')
+
+    # torch.save(model.state_dict(), f'model_epoch_{epoch+1}.pth')
+    # print(f'Model saved as model_epoch_{epoch+1}.pth')
