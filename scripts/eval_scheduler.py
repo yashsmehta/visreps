b440d0d7f0d48e02178838cfaa4a426739da1d52
Yash Mehta <yashsmehta95@gmail.com>
2024-05-04 11:31:14 -0400

    separate schedulers for train and eval

--- scripts/eval_scheduler.py
+++ scripts/eval_scheduler.py
@@ -1,35 +1,28 @@
 import subprocess
 import itertools
+import numpy as np
 import re
 
-exec_file = "archvision/run.py"
-seeds = 3
+exec_file = "archvision/run_eval.py"
+seeds = 1 
 
+queue, cores, use_gpu = "gpu_rtx8000", 6, True
 # queue, cores, use_gpu = "gpu_rtx", 5, True
-queue, cores, use_gpu = "gpu_a100", 12, True
+# queue, cores, use_gpu = "gpu_a100", 12, True
+# queue, cores, use_gpu = "gpu_h100", 12, True
 # queue, cores, use_gpu = "local", 4, False
 
 if(queue == "local" and use_gpu == True):
     raise Exception ("No GPUs available on this partition!")
 
 configs = {
-    "exp_name": ["arch_params"],
-    "model.nonlin": ["none", "relu", "elu", "tanh", "sigmoid"],
-    "model.weights_init": ["kaiming", "kaiming_uniform", "xavier", "xavier_uniform", "gaussian", "uniform"],
-    "model.norm": ["none", "batch", "channel", "instance"],
-    "log_expdata": [True],
+    "cfg_id": list(range(13, 31)),
+    "epoch": [0, 10, 20, 30, 40],
 }
 
-# configs = {
-#     "exp_name": ["benchmark"],
-#     "model.name": ["densenet121"],
-#     "model.pretrained": [True, False],
-#     "log_expdata": [True],
-# }
-# function to iterate through all values of dictionary:
+
 combinations = list(itertools.product(*configs.values()))
 
-# generate config string to pass to bash script
 use_gpu = str(use_gpu).lower()
 for combination in combinations:
     execstr = "python " + f"{exec_file}"
@@ -37,7 +30,6 @@ for combination in combinations:
         execstr += " " + key + "=" + str(combination[idx])
     cmd = ["scripts/submit_job.sh", str(cores), str(seeds), queue, execstr, use_gpu]
 
-    # Run the command and capture the output
     output = subprocess.check_output(
         cmd, stderr=subprocess.STDOUT, universal_newlines=True
     )
