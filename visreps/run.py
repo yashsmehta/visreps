3409979798b62849c56cec854c427779d86de166
Yash Mehta <yashsmehta95@gmail.com>
2025-02-19 18:00:41 -0500

    Add cfg validation in run and pretty formatting

--- visreps/run.py
+++ visreps/run.py
@@ -5,14 +5,17 @@ import visreps.utils as utils
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Training and evaluation script')
-    parser.add_argument('--mode', choices=['train', 'eval'], default='eval')
-    parser.add_argument('--config', default=None)
-    parser.add_argument('--override', nargs='*', default=[])
-    
+    parser = argparse.ArgumentParser(description="Training and evaluation script")
+    parser.add_argument("--mode", choices=["train", "eval"], default="eval")
+    parser.add_argument("--config", default=None)
+    parser.add_argument("--override", nargs="*", default=[])
+
     args = parser.parse_args()
-    cfg = utils.load_config(args.config or f"configs/{args.mode}/base.json", args.override)
-    
+    cfg = utils.load_config(
+        args.config or f"configs/{args.mode}/base.json", args.override
+    )
+    cfg = utils.validate_config(cfg)
+
     if cfg.mode == "train":
         trainer = Trainer(cfg)
         trainer.train()
