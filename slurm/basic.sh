85042d67bf9e7d2ef44cbbbcb0a58545a6581d5f
Yash Mehta <yashsmehta95@gmail.com>
2025-03-03 15:45:24 -0500

    Separate Slurm folder for scripts.

--- slurm/basic.sh
+++ slurm/basic.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=visreps
-#SBATCH --output=slurm_logs/test_%j.out
-#SBATCH --error=slurm_logs/test_%j.err
+#SBATCH --output=slurm/slurm_logs/test_%j.out
+#SBATCH --error=slurm/slurm_logs/test_%j.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:1
-#SBATCH --time=0:30:00
-#SBATCH --partition=v100
+#SBATCH --time=10:00:00
+#SBATCH --partition=a100
 #SBATCH --qos=qos_gpu
 #SBATCH --account=mbonner5_gpu
 
@@ -15,6 +15,12 @@ source .venv/bin/activate
 
 # Print some debug information
 echo "Running on node: $(hostname)"
+echo "CPU information:"
+lscpu | grep "Model name"
+echo "Memory information:"
+free -h
+echo "Disk information:"
+df -h /scratch4/mbonner5/shared/imagenet
 echo "GPU information:"
 nvidia-smi
 
