8b641ad14082a08fd5714c9b5418982ddfec82ec
Yash Mehta <yashsmehta95@gmail.com>
2026-01-23 14:54:15 -0500

    Consolidate to single plotter utils file

---
+++ plotters/utils/__init__.py
@@ -0,0 +1,9 @@
+# Plotting utilities
+from .plotter_utils import (
+    avg_over_subject_idx,
+    avg_over_seed,
+    avg_over_subject_idx_seed,
+    split_and_select_df,
+    get_best_layer_scores,
+    plot_brain_score_barplot,
+)
